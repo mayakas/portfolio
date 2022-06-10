c3dec2f4f1ea8301d1586a51f2dbcad130345e7e
sauromay <justineislearning@gmail.com>
2022-06-10 13:07:55 +0300

    navigation clearing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Navigation from "./components/Navigation";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
