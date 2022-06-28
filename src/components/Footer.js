60cb0d351f6a8cb29a85f3177969c5333e2988e6
sauromay <justineislearning@gmail.com>
2022-06-28 15:26:30 +0300

    dynamic year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 function Footer() {
+  let today = new Date();
+  let year = today.getFullYear();
+
   return (
     <footer class="footer fixed-bottom py-3 bg-light">
       <div class="container mx-auto">
         <div class="d-flex justify-content-center">
-          <div class="mx-1">developer's joint</div>
+          <div class="mx-1 text-muted">developer's joint</div>
           <div class="mx-1">•</div>
-          <div class="mx-1">© 2022</div>
+          <div class="mx-1 text-muted">© {year}</div>
         </div>
       </div>
     </footer>
